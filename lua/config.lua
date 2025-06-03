6993007e2e07b731e12d3d46d022b36db71063c3
Enrico Panetta <convezione@proton.me>
2025-06-03 11:14:48 +0200

    Download random image to file

--- lua/config.lua
+++ lua/config.lua
@@ -6,12 +6,15 @@ M.default.url = "https://picsum.photos"
 M.default.width = 1920
 M.default.height = 1080
 
-M.join = function (oldCfg, newCfg) 
+M.utils.join = function (oldCfg, newCfg) 
 	for k,v in pairs(newCfg) do
 		oldCfg[k] = v
 	end
 	return oldCfg
 end
 
+M.utils.computeUrl = function(cfg)
+	return string.format("%s/%d/%d", cfg.url, cfg.width, cfg.height)
+end
 
 return M
--- lua/nvim_randbg.lua
+++ lua/nvim_randbg.lua
@@ -4,7 +4,7 @@ M.setup = function (cfg)
 	cfg = cfg or {}
 	local config = require('config')
 	-- Join input config with default one
-	local newCfg = config.join(config.default, cfg)
+	local newCfg = config.utils.join(config.default, cfg)
 	
 	local imgHandler = require('image')
 	imgHandler.getImage(newCfg)
